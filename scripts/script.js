a98ef3765e75121592f0f2fea450cff99781da8f
Michael MacLean <Mike.maclean95@gamil.com>
2017-08-08 13:07:42 -0400

    Added basic functionality

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+var gridSize = 16
+var cubeSize = 436/(gridSize);
+
+$(document).ready(function(){
+  for(var x = 0; x <= gridSize-1; x++) {
+    for(var i = 0; i <= gridSize-1; i++) {
+      $(".container").append("<div class='sketch'></div>");
+    //  $(".container").css("grid-template-columns","(" + gridSize + "," +
+      //  cubeSize + "px");
+      //$(".container").css("grid-template-rows" , "(" + gridSize + "," +
+        //cubeSize + "px");
+
+      $(".sketch").css("height",cubeSize);
+      $(".sketch").css("width",cubeSize);
+
+    };
+  };
+  $(".sketch").hover(function(){
+    $(this).css("background-color","black");
+  });
+  $("button").click(function(){
+    $(".sketch").css("background-color","white");
+  });
+});
