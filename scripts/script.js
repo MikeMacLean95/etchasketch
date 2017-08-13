271f7df61ac3910448fd8cdeef66c0b8627bf970
Michael MacLean <Mike.maclean95@gamil.com>
2017-08-12 21:26:34 -0400

    Added functional grid change function as well as fixing random color generator

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,81 @@
 var gridSize = 16
 var cubeSize = 500/(gridSize);
-var black = true;
-$(document).ready(function(){
+var colorTile = 3;
 
-  // for loop to generate grid of cubes
+
+function gridGen(gridSize) {
+  cubeSize = 500/gridSize;
   for(var x = 0; x <= gridSize-1; x++) {
     for(var i = 0; i <= gridSize-1; i++) {
-      $(".container").append("<div class='sketch'></div>");
+      $(".container").append("<div class='sketch' data-count='0'></div>");
       $(".sketch").css("height",cubeSize);
       $(".sketch").css("width",cubeSize);
     };
   };
+};
 
-  //adds color to hover the else is the random color generator
-  $(".sketch").hover(function(){
-    if (black) {
+function incrementalDark(clAss, alpha) {
+
+
+  $(clAss).css("background-color", "rgba(0,0,0," + alpha/10 + ")");
+  console.log(alpha);
+
+};
+
+
+function hoverChange() {
+  $(".sketch").mouseenter(function(){
+
+    console.log(count);
+    if (colorTile === 1) {
         $(this).css("background-color","black");
-    } else {
+    } else if (colorTile === 2) {
+  // This is the random color generator
         var r = Math.floor(Math.random()*255);
         var g = Math.floor(Math.random()*255);
         var b = Math.floor(Math.random()*255);
         $(this).css("background-color", "rgb(" + r + "," + g + "," + b + ")")
+    } else {
+  //This increases darkness of black by 10%
+
+          incrementalDark(this, count);
+
     };
   });
 
+}
+
+function clear () {
+  $(".sketch").css("background-color","white");
+};
+
+function gridPrompt(){
+  gridSize = prompt("Enter Number of Cubes in a Row:");
+  clear();
+  $(".sketch").remove();
+  gridGen(gridSize);
+  hoverChange();
+};
+
+
+
+$(document).ready(function(){
+
+  // for loop to generate grid of cubes
+  gridGen(gridSize);
+
+  //adds color to hover colorTIle 1 is standard black
+  hoverChange();
   //buttons
   $("#color").click(function(){
-    if(black){black = false;} else {black = true;}});
+    if(colorTile === 1){
+      colorTile = 2;
+    } else if (colorTile === 2){
+      colorTile = 1;
+    } else if (colorTile === 3){
+      colorTile = 1;
+    }});
   $("#reset").click(function(){
-    $(".sketch").css("background-color","white");
+    clear();
   });
 });
